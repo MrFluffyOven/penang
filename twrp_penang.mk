05429d5b9f385397a61d8d159d2dcdc633453ade
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-26 14:53:18 +0000

    remove junk

--- twrp_penang.mk
+++ twrp_penang.mk
@@ -20,10 +20,3 @@ PRODUCT_NAME := twrp_penang
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Holi for arm64
 PRODUCT_MANUFACTURER := motorola
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="penang_g-user 11 T1TP33.1-47-2 76390 release-keys"
-
-BUILD_FINGERPRINT := motorola/penang_g/penang:11/T1TP33.1-47-2/76390:user/release-keys
