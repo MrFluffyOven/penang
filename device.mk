e3ffedb5500f9cc3e6fc9f15deda1a3bf257a1ab
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2024-01-26 14:55:29 +0000

    kernel update

--- device.mk
+++ device.mk
@@ -33,3 +33,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+    # Copy modules for depmod
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(LOCAL_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)    
+
